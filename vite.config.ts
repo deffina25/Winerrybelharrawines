8d4ac36f7e8d4bca9f7b5d4cbaeec20be2fbc2ef
megapihar6 <karvasarniy.sa@intechs.by>
2026-01-22 13:10:41 +0100

    add base url

--- vite.config.ts
+++ vite.config.ts
@@ -54,9 +54,9 @@
       target: 'esnext',
       outDir: 'build',
     },
-    server: {
-      port: 3000,
-      open: true,
-    },
+    // server: {
+    //   port: 3000,
+    //   open: true,
+    // },
   });
 
